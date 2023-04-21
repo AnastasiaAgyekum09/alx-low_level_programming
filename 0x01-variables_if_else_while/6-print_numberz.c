f7b7047cc7a025cfb6501f67643438ea71a85485
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-21 13:39:59 -0700

    Base ten numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print all sinle numbers of base ten
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+
+	return (0);
+}
