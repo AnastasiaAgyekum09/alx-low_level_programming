6fce7eefb7ba25588d3076b4d0fc5605033e5b30
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-22 03:36:42 -0700

    Combination of possible two two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,13 +11,14 @@ int main(void)
 	{
 		for (num2 = 0; num2 <= 99; num2++)
 		{
-			if (num2 != num1)
+			if (num2 !=  num1)
 			{
 				putchar((num1 / 10) + 48);
 				putchar((num1 % 10) + 48);
 				putchar(' ');
 				putchar((num2 / 10) + 48);
 				putchar((num2 % 10) + 48);
+
 				if (num1 != 98 || num2 != 99)
 				{
 					putchar(',');
