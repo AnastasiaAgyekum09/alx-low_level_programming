4c2747b82b7b2929e9e1a4bdc598fa30f674df8f
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-26 01:53:55 -0700

    Know your 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,33 +10,22 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		_putchar(48);
+		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
+			_putchar(44);
+			_putchar(32);
 
-			if ((c / 10) == 0)
+			if (c <= 9)
 			{
-				if (b != 0)
-				{
-				_putchar(' ');
-				_putchar(c + '0');
-				}
-				if (b == 9)
-				{
-					continue;
-				_putchar(',');
-				_putchar(' ');
-				}
+				_putchar(32);
+				_putchar(c + 48);
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b == 9)
-				{	continue;
-				_putchar(',');
-				_putchar(' ');
-				}
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
 			}
 		}
 		_putchar('\n');
