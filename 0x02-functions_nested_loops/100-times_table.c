38c240a4f1e138141e50bd1aea7280ffb678dac4
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-26 03:49:00 -0700

    Number times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,32 +12,36 @@ void print_times_table(int n)
 	{
 		for (x = 0; x <= n; x++)
 		{
-			_putchar(48);
-		}
-		for (y = 1; y <= n; y++)
-		{
-			z = x * y;
-			_putchar(44);
-			_putchar(32);
-		}
-		if (z <= 9)
-		{
-			_putchar(32);
-			_putchar(z + 48);
-		}
-		else if (z <= 99)
-		{
-			_putchar(32);
-			_putchar((z / 10) + 48);
-			_putchar((z % 10) + 48);
-		}
-		else
-		{
-			_putchar(((z / 100) % 10) + 48);
-			_putchar(((z / 10) % 10) + 48);
-			_putchar((z % 10) + 48);
+			for (y = 0; y <= n; y++)
+			{
+				z = y * x;
+				if (y == 0)
+				{
+					_putchar(z + 0);
+				} else if (z < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + 0);
+				} else if (z >= 10 && z < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((z / 10) + '0');
+					_putchar((z % 10) + '0');
+				} else if (z >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
-
