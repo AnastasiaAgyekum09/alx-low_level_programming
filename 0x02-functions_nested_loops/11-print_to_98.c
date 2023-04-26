ee261fadcaa75838a14abd92c51d24a532636130
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-26 02:43:50 -0700

    Natural numbers with new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,19 +6,19 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n <= 98)
 	{
-		for (; n > 98; n--)
+		for (n = n; n <=  97; n++)
 		{
 			printf("%d,", n);
 		}
 	}
-	else if (n < 98)
+	else
 	{
-		for (; n < 98; n++)
+		for (n = n; n > 98; n--)
 		{
 			printf("%d,", n);
 		}
 	}
-	printf("%d\n", n);
+	printf("98\n");
 }
