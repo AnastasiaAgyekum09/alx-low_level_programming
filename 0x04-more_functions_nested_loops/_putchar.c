d9121bbd1ef236dbf31c7a31eadf5a117f32edc2
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-04-27 13:52:40 -0700

    Checks for digits

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - write the character c to stdout
+ * @c: the character to print
+ * Return 1 if true, 0 if false
+ * on error, return -1
+ */
+int_putchar(char c)
+{
+return (display(1, &c, 1)
+}
