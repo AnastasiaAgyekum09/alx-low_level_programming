ab372e2b50a8a7ffa7d7b06e69ea0758396d5912
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-02 13:30:46 -0700

    Reverse the string without new line

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string without new line
+ * @s: string to reverse
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int string = 0;
+	int n;
+
+	while (s[string] != '\0')
+	{
+		string++;
+		for (n = 0; n < string; n++)
+		{
+			string--;
+			reverse = s[n];
+			s[n] = s[string];
+			s[string] = reverse;
+		}
+	}
+}
