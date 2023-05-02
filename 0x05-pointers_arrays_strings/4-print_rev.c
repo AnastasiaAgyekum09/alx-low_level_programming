85dbdd8ca5b9f3cdf078c36cf7588041086ced17
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-02 14:51:55 -0700

    Reverse string with new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,10 @@ void print_rev(char *s)
 {
 	int x = 0;
 
-	while (*s[x] != '\0')
+	while (*s != '\0')
 		x++;
 	for (x = x - 1; x >= 0; x--)
-		_putchar(s[x]);
+		_putchar(*s);
 
 	_putchar('\n');
 }
