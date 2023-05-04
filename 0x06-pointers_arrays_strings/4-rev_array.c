9923304c58778762cd95a555aa67963b38714c8d
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-04 09:45:55 -0700

    Reverse number of elements in an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the number of elements in a content
+ * of an array of integers
+ * @a: array of integers
+ * @n: number of integers to reverse
+ * Return: null
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = 0; x < n; x++)
+	{
+		n--;
+		y = 0;
+		a[x] = a[n];
+		a[n] = y;
+	}
+}
+
