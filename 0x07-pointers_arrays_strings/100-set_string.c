88c140f82db00273585a40f7c8f40927bf37d0f1
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-08 13:48:49 -0700

    Pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Write a function that sets the value of a pointer to a char.
+ * @s: from one pointer to another
+ * @to: char pointer
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
