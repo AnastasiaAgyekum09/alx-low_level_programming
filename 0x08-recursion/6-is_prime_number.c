3897d9487b54ee54f8f6fed1ef6a74b6f236d471
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-10 10:59:37 -0700

    Prime numbers and recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+int actual_prime(int n, int x);
+
+/**
+ * is_prime_number - display prime numbers
+ * @n: input integers
+ * Return: 1 on success, 0 on error
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+/**
+ * actual_prime - display recursive prime numbers
+ * @n: input integers
+ * @x: iterator
+ * Return: 1 0n success, 0 on error
+ */
+
+int actual_prime(int n, int x)
+{
+	if (n % x == 0 && x > 0)
+		return (0);
+	if (x == 1)
+		return (1);
+	return (actual_prime(n, x - 1));
+}
