c409e10f0016d469739d6bda49d345012ca2fff0
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-10 13:28:54 -0700

    Identical strings only

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,10 +9,6 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*')
-	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
 	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
@@ -21,9 +17,13 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
-	else if (*s2 == *s1)
+	if (*s2 == '*')
 	{
-		return (wildcmp(s2 + 1, s1 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
