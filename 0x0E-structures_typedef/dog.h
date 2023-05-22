862202d9a8b63b5d3b296130b6aea563bdccc9b2
AnastasiaAgyekum09 <anastasiaagyekum09@gmail.com>
2023-05-22 10:30:56 -0700

    Struc dog type for my fav dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,7 +19,6 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -27,4 +26,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
